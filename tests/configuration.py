785d387d964027c1e47b62b0feecc767add34d90
Grant Blinkov <blinkovgd@yandex.ru>
2024-07-18 02:21:47 +0300

    Добавлены константы registration and login (URL)

--- tests/configuration.py
+++ tests/configuration.py
@@ -5,6 +5,8 @@ import random
 EMAIL = "GrantBlinkov11776@yandex.ru"
 PASSWORD = "Qwe123Asd456!"
 URL = "https://stellarburgers.nomoreparties.site/"
+URL_REGISTRATION = "https://stellarburgers.nomoreparties.site/register"
+URL_LOGIN = "https://stellarburgers.nomoreparties.site/login"
 N = 13
 res = ''.join(random.choices(string.ascii_letters, k=N))
 
