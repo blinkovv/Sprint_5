f9902cb8b39fedad12c8244ccf74b4840fc48f77
Grant Blinkov <blinkovgd@yandex.ru>
2024-07-18 16:20:05 +0300

    Переписал все локаторы, уйдя от root

--- tests/locators.py
+++ tests/locators.py
@@ -1,27 +1,29 @@
-input_name = '//*[@id="root"]/div/main/div/form/fieldset[1]/div/div/input'  # Регистрация. Поле Имя
-input_email = '//*[@id="root"]/div/main/div/form/fieldset[2]/div/div/input'  # Регистрация. Поле Email
-input_password = '//*[@id="root"]/div/main/div/form/fieldset[3]/div/div/input'  # Регистрация. Поле Пароль
-button_register = '//*[@id="root"]/div/main/div/form/button'  # Регистрация. Кнопка "Зарегистрироваться"
-error_password = '//*[@id="root"]/div/main/div/form/fieldset[3]/div/p'  # Регистрация. Некорректный пароль ошибка текст
+input_name = "//label[text()='Имя']/parent::div/input"  # Регистрация. Поле Имя
+input_email = "//label[text()='Email']/parent::div/input"  # Регистрация. Поле Email
+input_password = "//label[text()='Пароль']/parent::div/input"  # Регистрация. Поле Пароль
+button_register = '//button[text()="Зарегистрироваться"]'  # Регистрация. Кнопка "Зарегистрироваться"
+error_password = '//p[text()="Некорректный пароль"]'  # Регистрация. Некорректный пароль ошибка текст
 
 
-button_login = '//*[@id="root"]/div/main/section[2]/div/button'  # Кнопка "Войти" на главной странице
-button_login_in_chapter = '//*[@id="root"]/div/main/div/form/button'  # Кнопка "Войти" в разделе ВХОД
-input_password_in_enter_page = '//*[@id="root"]/div/main/div/form/fieldset[2]/div/div/input'  # Поле "Пароль" на странице ВХОДА
-button_personal_account = '//*[@id="root"]/div/header/nav/a/p'  # Кнопка "Личный Кабинет" на главной странице
-button_login_from_register = '//*[@id="root"]/div/main/div/div/p/a'  # Кнопка "Войти" в форме регистрации и Восстановить пароль
-text_entrance_on_the_login_page = '//*[@id="root"]/div/main/div/h2' # Текст "Вход" на странице Login
-place_an_order_button = '//*[@id="root"]/div/main/section[2]/div/button' # Кнопка Оформить заказ для авторизованного пользователя
+button_login = '//button[text()="Войти в аккаунт"]'  # Кнопка "Войти" на главной странице
+button_login_in_chapter = '//button[text()="Войти"]'  # Кнопка "Войти" в разделе ВХОД
+button_reset_password = 'a[href="/forgot-password"]'  # Кнопка "Восстановить пароль" на странице Логин
+input_password_in_enter_page = "//label[text()='Пароль']/parent::div/input"  # Поле "Пароль" на странице ВХОДА
+button_personal_account = '//p[text()="Личный Кабинет"]'  # Кнопка "Личный Кабинет" на главной странице
+button_login_from_register = '//a[text()="Войти"]'  # Кнопка "Войти" в форме регистрации и Восстановить пароль
+text_entrance_on_the_login_page = '//h2[text()="Вход"]'  # Текст "Вход" на странице Login
+text_assemble_burger_main_page = '//h1[text()="Соберите бургер"]'  # Тескст "Собери бургер" на главной странице
+place_an_order_button = '//button[text()="Оформить заказ"]'  # Кнопка Оформить заказ для авторизованного пользователя
 
 
-button_constructor = '//*[@id="root"]/div/header/nav/ul/li[1]/a/p' # Кнопка "Конструктор" переход в его вкладку
-button_logotype = '#root > div > header > nav > div > a > svg'  # Переход по Stellar Burger
-button_log_out = '//*[@id="root"]/div/main/div/nav/ul/li[3]/button'  # Кнопка "Выход" из личного аккаунта
+button_constructor = '//p[text()="Конструктор"]'  # Кнопка "Конструктор" переход в его вкладку
+button_logotype = '//a[@class="active"]' # Переход по Stellar Burger
+button_log_out =  '//button[text()="Выход"]' # Кнопка "Выход" из личного аккаунта
 
 
-link_buns = '//*[@id="root"]/div/main/section[1]/div[1]/div[1]'  # Раздел Булки
-link_sauces = '//*[@id="root"]/div/main/section[1]/div[1]/div[2]'  # Раздел Соусы
-link_fillings = '//*[@id="root"]/div/main/section[1]/div[1]/div[3]'  # Раздел Начинки
-text_buns = '//*[@id="root"]/div/main/section[1]/div[2]/h2[1]'  # Текст-Заголовок в разделе Булки
-text_sauces = '//*[@id="root"]/div/main/section[1]/div[2]/h2[2]' # Текс-Заголовок в разделе Соусы
-text_fillings = '//*[@id="root"]/div/main/section[1]/div[2]/h2[3]'  # Текст-Заголовок в разделе Начинки
+link_buns = '//span[text()="Булки"]'  # Раздел Булки
+link_sauces = '//span[text()="Соусы"]'  # Раздел Соусы
+link_fillings = '//span[text()="Начинки"]'  # Раздел Начинки
+text_buns = '//h2[text()="Булки"]'  # Текст-Заголовок в разделе Булки
+text_sauces = '//h2[text()="Соусы"]'  # Текс-Заголовок в разделе Соусы
+text_fillings = '//h2[text()="Начинки"]'  # Текст-Заголовок в разделе Начинки
